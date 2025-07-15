eaa96480d0b6f07f45a6261557c0bc55f340cdb7
Joji31 <gogimogi01212@gmail.com>
2025-07-15 11:01:17 +0400

    Add files via upload

---
+++ output/networksystem_dll.hpp
@@ -0,0 +1,21 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-07-15 06:56:54.511724600 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace networksystem_dll {
+            // Parent: None
+            // Field count: 1
+            namespace ChangeAccessorFieldPathIndex_t {
+                constexpr std::ptrdiff_t m_Value = 0x0; // int32
+            }
+        }
+    }
+}
